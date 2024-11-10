c35513c0d8d75ce7c8af5a66f0c0f4abf75ff1ce
Vincent Yang <missuo@pm.me>
2024-11-09 19:21:35 -0500

    Initial commit

---
+++ steam.js
@@ -0,0 +1,19 @@
+/*
+[rewrite_local]
+# > Steam TOTP Extractor
+^https?:\/\/api\.steampowered\.com\/ITwoFactorService\/AddAuthenticator url script-response-body https://raw.githubusercontent.com/missuo/SteamGuardExtractor/refs/heads/main/steam.js
+[mitm] 
+hostname = api.steampowered.com
+*/
+
+var body = $response.body;
+const secretMatch = body.match(/secret=([A-Z0-9]+)/);
+if (secretMatch) {
+    const secret = secretMatch[1];
+    console.log("Secret:", secret);
+    $notify("Click to Copy Secret", {"update-pasteboard": secret});
+} else {
+    console.log("Secret not found");
+    $notify("Secret not found")
+}
+$done({body});
